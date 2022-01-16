7f6291d5ca0dcf67db51ef0eb068c0de5eb3a92b
mark.tornga@slalom.com <mark.tornga@slalom.com>
2022-01-16 17:35:21 -0600

    updates to launcher and chrontab

--- Sensor/launcher.sh
+++ Sensor/launcher.sh
@@ -3,7 +3,7 @@
 #launcher.sh
 #navigate to the home directory, then to this directory, then execute python script, then back home
 cd /
-cd /home/pi/repos/piot
-sudo python3 SenseAndWriteToSQLite.py bedroom
-sudo python3 PiOTFlask.py 192.168.1.16
+cd /home/pi/repos/PiSensorNetwork/Sensor
+sudo python3 SenseAndWriteToSQLite.py nursery 3
+sudo python3 PiOTFlask.py 192.168.1.209
 cd /
